09f4b3d2625505cc4f78c9f23070017d112a0075
enoch-eeb <henokh.mikhael@gmail.com>
2025-10-24 23:43:42 +0700

    ngrok integrate

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -63,7 +63,7 @@ async function cekHoax() {
 
   try {
     //fetch API
-     const response = await fetch("http://127.0.0.1:5000/predict", {
+     const response = await fetch("https://untransmigrated-unimputable-kelli.ngrok-free.dev/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ 
