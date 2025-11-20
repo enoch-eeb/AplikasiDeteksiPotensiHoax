efd8186e602a70c99acb2f138236b3e86793dfe9
Henokh Mikhael <73613963+enoch-eeb@users.noreply.github.com>
2025-11-20 10:48:25 +0700

    Update API Fetch

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -63,7 +63,7 @@ async function cekHoax() {
 
   try {
     //fetch API
-     const response = await fetch("/assets/api/predict.js", {
+     const response = await fetch("/api/predict", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
